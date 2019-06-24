4b08354bc8b0f408bc6ca05a011388946c66a064
RhysRead <30420852+RhysRead@users.noreply.github.com>
2019-06-24 09:29:03 +0100

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""main.py: The main folder for the execution of the CarSoft project."""
+
+__author__ = "Rhys Read"
+__copyright__ = "Copyright 2019, Rhys Read"
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class Main(object):
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.start()
