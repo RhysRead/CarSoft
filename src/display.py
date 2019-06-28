a0b867f0a40d3bc78d4fb81688fd4c232c3a1dce
Rhys <rhysread0@gmail.com>
2019-06-28 14:02:55 +0100

    Fixed important bugs

--- src/display.py
+++ src/display.py
@@ -16,7 +16,9 @@ class DisplayManager(object):
         self.__root.pack_propagate(0)
         self.__root.resizable(0, 0)
         # Initial geometry declaration
-        self.__root.geometry('1000x800+0+0')
+        self.base_geometry = '1000x800+0+0'
+        self.__root.geometry(self.base_geometry)
+        self.__root.attributes('-fullscreen', False)
 
         self.__main_frame = MainFrame(self.__root, self)
 
@@ -27,6 +29,9 @@ class DisplayManager(object):
             self.__current_frame_class.pack_forget()
         frame.pack()  # Todo: Consider using fill and expand here for proper scaling
         self.__current_frame_class = frame
+
+    def get_frame_class(self):
+        return self.__current_frame_class
 
     def start(self):
         self.set_frame_class(self.__main_frame)
@@ -58,9 +63,16 @@ class MainFrame(FrameClass):
     def __init__(self, root: tk.Tk, display_manager: DisplayManager):
         super().__init__(root, display_manager)
 
-        self._geom = "{0}x{1}+0+0".format(
-            self._root.winfo_screenwidth(), self._root.winfo_screenheight())
-        self._full = False
+        window_geometry = clean_geometry(self._root.winfo_geometry())
+
+        if window_geometry == self._display_manager.base_geometry\
+                or window_geometry == '1x1+0+0':
+            self._geom = "{0}x{1}+0+0".format(
+                self._root.winfo_screenwidth(), self._root.winfo_screenheight())
+            self._full = False
+        else:
+            self._geom = self._display_manager.base_geometry
+            self._full = True
 
     def _create(self):
         self.__label0 = tk.Label(self._frame,
@@ -121,17 +133,18 @@ class MainFrame(FrameClass):
         self._display_manager.set_frame_class(ConfigManagerFrame(self._root, self._display_manager))
 
     def __toggle_full_screen(self):
-        # Toggling fullscreen attribute
-        self._full = not self._full
-        self._root.attributes('-fullscreen', self._full)
-
         # Toggling window geometry
         geom = self._root.winfo_geometry()
         # Replacing the position value of the window to fix a weird bug
-        geom = geom.split('+')[0] + '+0+0'
+        geom = clean_geometry(geom)
         logging.info('Switching window size from {} to {}.'.format(geom, self._geom))
         self._root.geometry(self._geom)
         self._geom = geom
+
+        # Toggling fullscreen attribute
+        self._full = not self._full
+        self._root.attributes('-fullscreen', self._full)
+        self._root.update()
 
 
 class LiveDisplayFrame(FrameClass):
@@ -174,3 +187,12 @@ class ConfigManagerFrame(FrameClass):
                                    width='10',
                                    height='5')
         self.__button0.grid(row=0, column=1, sticky=tk.E, padx=100)
+
+
+def clean_geometry(geometry: str):
+    """
+    Removes the position value at the end of a geometry value.
+    :param geometry:
+    :return:
+    """
+    return geometry.split('+')[0] + '+0+0'
